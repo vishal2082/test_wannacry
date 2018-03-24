f0f8860145807c0d2ea07518dec83d4ae9fc35b2
vishal2082 <vishalbhardwaj070@gmail.com>
2018-03-24 22:47:58 +0530

    commit for test_wannacry

---
+++ src/main/java/test_wannacry/App.java
@@ -0,0 +1,11 @@
+package test_wannacry;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Maven Project...");
+
+	}
+
+}
---
+++ src/test/java/com/App/AppTest.java
@@ -0,0 +1,14 @@
+package com.App;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
